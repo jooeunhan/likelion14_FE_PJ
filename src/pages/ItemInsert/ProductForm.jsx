12a5e249c6d7e044a75194b5a8c08e131b704c19
jooeunhan <yeppijune@gmail.com>
2026-05-14 19:58:24 +0900

    :art: Design: 상품 사진 Wrapper에 맞게 변경

--- src/pages/ItemInsert/ProductForm.jsx
+++ src/pages/ItemInsert/ProductForm.jsx
@@ -29,7 +29,7 @@ const UploadIcon = styled.img`
 const ProductPreviewImg = styled.img`
   width: 100%;
   height: 100%;
-  object-fit: contain;
+  object-fit: cover;
 `;
 
 export function ImageUploader({ defaultImage }) {
